1d23ee7a125f0eb965c120eccc4f592ce3d265c1
gpavithra673 <93427264+gpavithra673@users.noreply.github.com>
2023-03-18 11:24:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        bird Bd= new bird();
+        Bd.walk();
+        Bd.fly();
+        Bd.sing();
+    }
+}
---
+++ bird.java
@@ -0,0 +1,8 @@
+public class bird extends animal{
+    public void fly(){
+        System.out.print("I m flying");
+    }
+    public void sing(){
+        System.out.print(" "+"I m singing");
+    }
+}
