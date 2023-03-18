e8af68e9f7a8d61a33792eba786917410f09f998
gpavithra673 <93427264+gpavithra673@users.noreply.github.com>
2023-03-18 11:25:59 +0530

    Delete bird.java

--- bird.java
+++
@@ -1,8 +0,0 @@
-public class bird extends animal{
-    public void fly(){
-        System.out.print("I m flying");
-    }
-    public void sing(){
-        System.out.print(" "+"I m singing");
-    }
-}
