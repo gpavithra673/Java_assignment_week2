12c91e739def78d1954d9defdcd28f364a89cb96
gpavithra673 <93427264+gpavithra673@users.noreply.github.com>
2023-03-18 12:55:00 +0530

    Add files via upload

---
+++ q1/Animal.java
@@ -0,0 +1,8 @@
+package q1;
+
+public class Animal {
+    public void walk(){
+        System.out.println("I m walking");
+    }
+}
+
---
+++ q1/Bird.java
@@ -0,0 +1,11 @@
+package q1;
+
+public class Bird extends Animal{
+    public void fly(){
+        System.out.print("I m flying");
+    }
+    public void sing(){
+        System.out.print(" "+"I m singing");
+    }
+}
+
